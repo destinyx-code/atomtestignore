3def0cacf79af26343a11baf5af16e9227a2ceb6
Ryder Stanley <ryderstanley2007@gmail.com>
2020-01-18 22:25:17 -0700

    added the code

---
+++ 22.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace 22;
+{
+
+}
+
+static void Main(string[] args)
+{
+
+  Console.WriteLine("What is 2 + 2?");
+
+    int answer = 4;
+
+      if (answer = 4)
+        {
+
+          Console.WriteLine("Correct!");
+
+            else if (answer != 4)
+
+            {
+
+              Console.WriteLine("You fuckin dumb lol");
+
+            }
+
+            {
+
+            }
+
+        }
+}
